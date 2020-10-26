c4dce2c56b3480cad4b952380a0d3c690e36f821
Evrim Cabuk <evrim.cabuk@wearekettle.com>
2020-10-26 16:32:59 +0000

    My awesome Lando-based changes

--- web/wp-content/plugins/trevor/static/js/blocks.js
+++ web/wp-content/plugins/trevor/static/js/blocks.js
@@ -1,2 +1,2 @@
-(this.webpackJsonptrevor=this.webpackJsonptrevor||[]).push([[2],{31:function(o,p){}},[[31,0]]]);
+(this.webpackJsonptrevor=this.webpackJsonptrevor||[]).push([[2],{33:function(o,p){}},[[33,0]]]);
 //# sourceMappingURL=blocks.js.map
