19581a9b4681e348bb61aa400969e20a221a28fb
Evrim Cabuk <evrim.cabuk@wearekettle.com>
2021-01-15 14:08:01 +0000

    My awesome Lando-based changes

--- web/wp-content/plugins/wpsolr-search-engine/wpsolr/core/classes/extensions/premium/includes/indexing-post-types.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$posts = get_post_types();
---
+++ web/wp-content/themes/trevor/lib/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7040818dffd4bffa086837c50175d6818498b195',
+    'name' => 'kettle/trevor-wp-theme',
+  ),
+  'versions' => 
+  array (
+    'kettle/trevor-wp-theme' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7040818dffd4bffa086837c50175d6818498b195',
+    ),
+  ),
+);
