4c75ef9be0d89ebf0082e03528c8617b6c87fa5d
Anthony Bovasso <anthony.bovasso@codeandtheory.com>
2021-06-22 11:09:57 +0000

    My awesome Lando-based changes

---
+++ web/wp-content/themes/trevor/lib/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1c921bb777927109043f1e2438dda006350f467c',
+    'name' => 'kettle/trevor-wp-theme',
+  ),
+  'versions' => 
+  array (
+    'kettle/trevor-wp-theme' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c921bb777927109043f1e2438dda006350f467c',
+    ),
+  ),
+);
