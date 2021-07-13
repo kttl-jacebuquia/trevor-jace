6f386e240997272780ce3b4b1d789f1d4790a461
Joshua Cerbito <joshua.cerbito@codeandtheory.com>
2021-07-13 12:33:00 +0000

    🚀 Deploy to STG 2021.07.13

--- web/wp-content/plugins/trevor/lib/composer/installed.php
+++ web/wp-content/plugins/trevor/lib/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'ea19ccb6dad8a1bab3021aab2fe314a6fec8f003',
+    'reference' => 'e2195236de8721f1ff89e992e47a00bda0c4e072',
     'name' => 'kettle/trevor-wp-plugin',
   ),
   'versions' => 
@@ -90,7 +90,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'ea19ccb6dad8a1bab3021aab2fe314a6fec8f003',
+      'reference' => 'e2195236de8721f1ff89e992e47a00bda0c4e072',
     ),
     'monolog/monolog' => 
     array (
