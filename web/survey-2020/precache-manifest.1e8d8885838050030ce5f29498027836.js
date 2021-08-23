5fac4d3ae5d08857eaea5d31e53a3aeea81d9334
hhsaito <hhsaito@gmail.com>
2021-08-22 18:10:08 -0700

    Added Survey 2020 and Survey 2021 files

---
+++ web/survey-2020/precache-manifest.1e8d8885838050030ce5f29498027836.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b1b8b856d7a8cb1cb0bae6d0573f2e9",
+    "url": "/survey-2020/static/media/Lato-Bold.5b1b8b85.ttf"
+  },
+  {
+    "revision": "be51fc27d2c4963abeb1",
+    "url": "/survey-2020/static/css/main.b999ccf7.chunk.css"
+  },
+  {
+    "revision": "7558290eb658272f6375",
+    "url": "/survey-2020/static/js/runtime~main.9ff92f76.js"
+  },
+  {
+    "revision": "dc032efaed9c68790a55",
+    "url": "/survey-2020/static/js/2.aadd6ca5.chunk.js"
+  },
+  {
+    "revision": "fe31856e1889ac2733a428db9946073f",
+    "url": "/survey-2020/static/media/logo.fe31856e.png"
+  },
+  {
+    "revision": "e90b75b41abd033b9b8d844aab21ab89",
+    "url": "/survey-2020/static/media/amit_headshot.e90b75b4.png"
+  },
+  {
+    "revision": "aadfda5779e58842c89bd7b677433ab0",
+    "url": "/survey-2020/static/media/amit_signature.aadfda57.svg"
+  },
+  {
+    "revision": "6d4e78225df0cfd5fe1bf3e8547fefe4",
+    "url": "/survey-2020/static/media/Lato-Regular.6d4e7822.ttf"
+  },
+  {
+    "revision": "91b510609446facfd45651b6c7460f5e",
+    "url": "/survey-2020/static/media/PT_Serif-Web-Regular.91b51060.ttf"
+  },
+  {
+    "revision": "be51fc27d2c4963abeb1",
+    "url": "/survey-2020/static/js/main.4bc56cb4.chunk.js"
+  },
+  {
+    "revision": "5c5f9f4495a8755de8bb985229030b92",
+    "url": "/survey-2020/static/media/AsapCondensed-Regular.5c5f9f44.ttf"
+  },
+  {
+    "revision": "a54bddbc1689d05277d2127f58589917",
+    "url": "/survey-2020/static/media/Lato-Black.a54bddbc.ttf"
+  },
+  {
+    "revision": "6c138bda49ad53b66b3a53e4a5b3f595",
+    "url": "/survey-2020/static/media/AsapCondensed-Bold.6c138bda.ttf"
+  },
+  {
+    "revision": "0850030b8646d6f15a73c0d44da5ff15",
+    "url": "/survey-2020/static/media/AsapCondensed-BoldItalic.0850030b.ttf"
+  },
+  {
+    "revision": "10f3dd9fc2958818c18c5ed2142df9e4",
+    "url": "/survey-2020/static/media/two-n-logo.10f3dd9f.svg"
+  },
+  {
+    "revision": "6a19f9aed9b718e2313e827339b49c81",
+    "url": "/survey-2020/static/media/link.6a19f9ae.svg"
+  },
+  {
+    "revision": "b12456c1c15616ee89eae3485919ce6d",
+    "url": "/survey-2020/static/media/facebook.b12456c1.svg"
+  },
+  {
+    "revision": "d06dfd0a364fb88ca9aa96251b915e2a",
+    "url": "/survey-2020/static/media/twitter.d06dfd0a.svg"
+  },
+  {
+    "revision": "392fa19dea31205fcba9a4be507b9a67",
+    "url": "/survey-2020/index.html"
+  }
+];
---
+++ web/survey-2021/precache-manifest.a5ab9a5e435ccc3e1f06cb3ac07f7eb9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c138bda49ad53b66b3a53e4a5b3f595",
+    "url": "/survey-2021/static/media/AsapCondensed-Bold.6c138bda.ttf"
+  },
+  {
+    "revision": "cbbf049ce13ca7073aac",
+    "url": "/survey-2021/static/css/main.12ac3c95.chunk.css"
+  },
+  {
+    "revision": "7558290eb658272f6375",
+    "url": "/survey-2021/static/js/runtime~main.9ff92f76.js"
+  },
+  {
+    "revision": "da1f0692ce83cc884a10",
+    "url": "/survey-2021/static/js/2.08048f43.chunk.js"
+  },
+  {
+    "revision": "fe31856e1889ac2733a428db9946073f",
+    "url": "/survey-2021/static/media/logo.fe31856e.png"
+  },
+  {
+    "revision": "e90b75b41abd033b9b8d844aab21ab89",
+    "url": "/survey-2021/static/media/amit_headshot.e90b75b4.png"
+  },
+  {
+    "revision": "aadfda5779e58842c89bd7b677433ab0",
+    "url": "/survey-2021/static/media/amit_signature.aadfda57.svg"
+  },
+  {
+    "revision": "0850030b8646d6f15a73c0d44da5ff15",
+    "url": "/survey-2021/static/media/AsapCondensed-BoldItalic.0850030b.ttf"
+  },
+  {
+    "revision": "5c5f9f4495a8755de8bb985229030b92",
+    "url": "/survey-2021/static/media/AsapCondensed-Regular.5c5f9f44.ttf"
+  },
+  {
+    "revision": "cbbf049ce13ca7073aac",
+    "url": "/survey-2021/static/js/main.2efe522b.chunk.js"
+  },
+  {
+    "revision": "91b510609446facfd45651b6c7460f5e",
+    "url": "/survey-2021/static/media/PT_Serif-Web-Regular.91b51060.ttf"
+  },
+  {
+    "revision": "a54bddbc1689d05277d2127f58589917",
+    "url": "/survey-2021/static/media/Lato-Black.a54bddbc.ttf"
+  },
+  {
+    "revision": "5b1b8b856d7a8cb1cb0bae6d0573f2e9",
+    "url": "/survey-2021/static/media/Lato-Bold.5b1b8b85.ttf"
+  },
+  {
+    "revision": "6d4e78225df0cfd5fe1bf3e8547fefe4",
+    "url": "/survey-2021/static/media/Lato-Regular.6d4e7822.ttf"
+  },
+  {
+    "revision": "10f3dd9fc2958818c18c5ed2142df9e4",
+    "url": "/survey-2021/static/media/two-n-logo.10f3dd9f.svg"
+  },
+  {
+    "revision": "6a19f9aed9b718e2313e827339b49c81",
+    "url": "/survey-2021/static/media/link.6a19f9ae.svg"
+  },
+  {
+    "revision": "b12456c1c15616ee89eae3485919ce6d",
+    "url": "/survey-2021/static/media/facebook.b12456c1.svg"
+  },
+  {
+    "revision": "d06dfd0a364fb88ca9aa96251b915e2a",
+    "url": "/survey-2021/static/media/twitter.d06dfd0a.svg"
+  },
+  {
+    "revision": "2c079b806ec83cc69666284e51e82b9a",
+    "url": "/survey-2021/index.html"
+  }
+];
