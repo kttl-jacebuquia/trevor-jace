0d17166a4ada37763d8c7bec909c22c134e6e8e1
Joshua Cerbito <joshua.cerbito@codeandtheory.com>
2021-08-23 09:47:00 +0000

    🚀 Deploy to DEV

---
+++ web/wp-content/themes/trevor/lib/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1e82e4db9e080a9014916019ebd0cb3d6adf5b39',
+    'name' => 'kettle/trevor-wp-theme',
+  ),
+  'versions' => 
+  array (
+    'kettle/trevor-wp-theme' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1e82e4db9e080a9014916019ebd0cb3d6adf5b39',
+    ),
+  ),
+);
