88a855b319bdb8177f7c8b93f9c5e6983d1e2d4a
hhsaito <hhsaito@gmail.com>
2021-09-13 11:15:59 -0700

    Altered  page template

--- web/wp-content/themes/trevor/blank.php
+++ web/wp-content/themes/trevor/blank.php
@@ -125,8 +125,6 @@ Template Name: Empty Template (no branding or foundation)
 <body id="tech">
 <?php while ( have_posts() ) : the_post(); ?>
 	<?php the_content(); ?>
-	
-
 <?php endwhile;?>
 <?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
 
