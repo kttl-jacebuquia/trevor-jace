17864a1608a4a6312396f6ecd3549928760708e4
hhsaito <hhsaito@gmail.com>
2021-09-14 13:51:53 -0700

    Removed GTM since it should load via wp_head

--- web/wp-content/themes/trevor/blank.php
+++ web/wp-content/themes/trevor/blank.php
@@ -5,23 +5,13 @@ Template Name: Empty Template (no branding or foundation)
 ?>
 
 <head>
-	<?php if ( $_ENV['PANTHEON_ENVIRONMENT'] === 'live' ) : ?>
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-WV5P347');</script>
-		<!-- End Google Tag Manager -->
-	<?php endif ?>
 
 	<?php wp_head(); ?>
-
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
-	
 </head>
+
 <body id="tech">
 <?php while ( have_posts() ) : the_post(); ?>
 	<?php the_content(); ?>
