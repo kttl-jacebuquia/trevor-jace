79e8f6c38dec01fb2fbb58aaeb003c0c373aa3e1
Joshua Sesniel Deloso <joshua.sesniel.deloso@codeandtheory.com>
2022-05-04 06:34:21 +0000

    Deploy in dev 2022-05-04 2:25 PM

--- web/wp-content/themes/trevor/build.php
+++ web/wp-content/themes/trevor/build.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php define( 'TREVORWP_STATIC_VERSION', 'b6d6b37d-6684-48bf-b578-4c062a1f5aad' );
-=======
-<?php define( 'TREVORWP_STATIC_VERSION', '3e83c1d3-393d-49b3-a77f-7a1f2def2749' );
->>>>>>> 0337883b4d9a5bea9b43275572bd721e2b9eb240
+<?php define( 'TREVORWP_STATIC_VERSION', 'c31737c6-0dce-4bfd-80ba-3057a4610033' );
